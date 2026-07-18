1e300d77c3addaf198265295f2e348d4f12966ec
pk3857703-oss <pk3857703@gmail.com>
2026-07-18 22:51:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+async function getWeather() {
+
+    const city = document.querySelector("#inp").value;
+
+    const response = await fetch(`http://localhost:3000/weather?city=${city}`);
+    const data = await response.json();
+
+    console.log(data); // IMPORTANT: check in browser console
+
+    if (data.cod == "404") {
+        document.querySelector("#show").innerHTML = "City Not Found!!";
+        return;
+    }
+
+    document.querySelector("#show").innerHTML = `
+      City: ${data.name}
+      Temp: ${data.main.temp}°C
+      Weather: ${data.weather[0].description}
+    `;
+}
+
+document.querySelector("#btn").addEventListener("click", (e) => {
+    e.preventDefault();
+    getWeather();
+});
+
+document.querySelector("#inp").addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+        getWeather();
+    }
+});
