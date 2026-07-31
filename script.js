f9144b9dfb503a98a0f8522f33821c7cb8331550
pk3857703-oss <pk3857703@gmail.com>
2026-07-31 23:10:10 +0530

    Fix temperature display and add swing animation
    Updated weather display to parse temperature correctly and added animation to the hero element.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function getWeather() {
 
     const city = document.querySelector("#inp").value;
 
-   const response = await fetch(`https://weather-checker-app-backend.onrender.com/weather?city=${city}`);
+      const response = await fetch(`https://weather-checker-app-backend.onrender.com/weather?city=${city}`);
     const data = await response.json();
 
     console.log(data); // IMPORTANT: check in browser console
@@ -14,7 +14,7 @@ async function getWeather() {
 
     document.querySelector("#show").innerHTML = `
       City: ${data.name}
-      Temp: ${data.main.temp}°C
+      Temp: ${parseFloat(data.main.temp)}°C <br>
       Weather: ${data.weather[0].description}
     `;
 }
@@ -29,3 +29,18 @@ document.querySelector("#inp").addEventListener("keyup", (e) => {
         getWeather();
     }
 });
+
+
+
+const hero = document.querySelector("h1");
+
+let angle = 0;
+let speed = 0.001; // swing speed
+
+function swing() {
+  angle = Math.sin(Date.now() * speed) * 10; // -10deg to +10deg
+  hero.style.transform = `rotate(${angle}deg)`;
+  requestAnimationFrame(swing);
+}
+
+swing();
